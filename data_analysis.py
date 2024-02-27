5dabd67c7e09d02894d7e3d737c48d7cdad83763
calagoo <robbierodriguez98@gmail.com>
2024-02-27 01:16:00 -0800

    updating csv

--- data_analysis.py
+++ data_analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import folium
+import folium.plugins
 
 # Specify the file path
 file_path = "D:\python\sfcrime_data\Police_Department_Incident_Reports__2018_to_Present_20240223.csv"
@@ -7,9 +8,19 @@ file_path = "D:\python\sfcrime_data\Police_Department_Incident_Reports__2018_to_
 # Read the CSV file into a pandas DataFrame
 df = pd.read_csv(file_path)
 
-# Create a scatter plot using Latitude and Longitude as x and y
-plt.scatter(df['Latitude'], df['Longitude'])
-plt.xlabel('Latitude')
-plt.ylabel('Longitude')
-plt.title('Scatter Plot of Data Points')
-plt.show()
+# Display the year 2024 data
+# df = df[df['Incident Year'] == 2024]
+
+# Create a folium map centered around San Francisco
+sf_map = folium.Map(location=[37.7749, -122.4194], zoom_start=12)
+
+# Add the heatmap layer to the map
+heat_data = [[row['Latitude'], row['Longitude']] for index, row in df.iterrows()]
+
+# if heat_data contains nan, remove it
+heat_data = [x for x in heat_data if str(x[0]) != 'nan' and str(x[1]) != 'nan']
+
+folium.plugins.HeatMap(heat_data).add_to(sf_map)
+
+# Display the map
+sf_map.save('heatmap_map.html')  # Save the map as an HTML file
