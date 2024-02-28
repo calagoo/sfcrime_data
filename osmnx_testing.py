2e0e851ea5426219a3170cc56cf823ba8ef68d93
calagoo <robbierodriguez98@gmail.com>
2024-02-28 01:19:56 -0800

    Added routing to osmnx

--- osmnx_testing.py
+++ osmnx_testing.py
@@ -1,10 +1,47 @@
 import osmnx as ox
+from osmnx import routing
+import matplotlib.pyplot as plt
 
-# Define the location (San Francisco)
-place_name = "San Francisco, California, USA"
 
-# Download the street network
-graph = ox.graph_from_place(place_name, network_type="drive")
+class Node:
+    def __init__(self, id, lat, lon):
+        self.id = id
+        self.lat = lat
+        self.lon = lon
+        self.nearest_node = None
 
-# Plot the street network
-ox.plot_graph(ox.project_graph(graph))
+    def __str__(self):
+        return "Node id: " + str(self.id) + " Lat: " + str(self.lat) + " Lon: " + str(self.lon)
+
+    def __repr__(self):
+        return str(self)
+    
+    def find_nearest_node(self, graph):
+        self.nearest_node = ox.nearest_nodes(graph, self.lon, self.lat)
+
+def main():
+
+    node1 = Node(1, 37.78338518997091, -122.43956939968092)
+    node2 = Node(2, 37.77571129565835, -122.42438491079076)
+    # Define the location (San Francisco)
+    place_name = "San Francisco, California, USA"
+
+    # Download the street network
+    graph = ox.graph_from_place(place_name, network_type="walk")
+
+
+    # Get the closest node
+    node1.find_nearest_node(graph)
+    node2.find_nearest_node(graph)
+
+    # Create a route between the nodes using the shortest path algorithm
+    route = routing.shortest_path(graph, node1.nearest_node, node2.nearest_node, weight="length")
+
+    # Plot the street network
+    if route:
+        ox.plot_graph_route(graph, route)
+    else:
+        print("No route found between the nodes.")
+
+if __name__ == "__main__":
+    main()
--- readme.md
+++ readme.md
@@ -3,8 +3,10 @@
 A walking or biking application that weighs high crime areas lower to keep the user in safer areas.
 
 # TODO (In somewhat order...):
-1. Use openstreet maps to determine closest node to a lat and lon point.
+1. Use openstreet maps to determine closest node to a lat and lon point. ✅
 2. Django hosted page to show map live.
+    -- Leaflet
+    -- OpenLayers
 3. Use webpage clicking interaction to choose nodes A and B, and return a route
 4. Implement crime data weighting for the route.
 5. Fine tune route.
@@ -12,3 +14,9 @@ A walking or biking application that weighs high crime areas lower to keep the u
 7. Implement searching an address -> node -> route? --- Maybe ---
 8. Expand to nearby areas, as long as data is present.
 9. Research what other map applications use? Osm? Google? Hodge Podge?
+
+
+# Saved Information For Future Processing:
+
+How mapping typically finds routes:
+The route finding process starts by obtaining the coordinates of points A and B. Then, it creates a virtual line connecting A and B and queries the database for road segments located within a specified distance from that line. If a path cannot be determined initially, the process is repeated with an increased distance.
